3a0e2e09ea391e4b9fb9b7f5042f545a587d1b9a
sneak-it <16104273+sneak-it@users.noreply.github.com>
2026-08-22 16:19:33 -0400

    reduce padding between items in blog list view

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -64,7 +64,7 @@ export default function BlogList({
         </nav>
       )}
 
-      <div className="space-y-12">
+      <div className="space-y-4">
         {posts.map((post, index) => (
           <m.article
             key={post.slug}
